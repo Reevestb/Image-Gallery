1e9aefcb8d96cbe9e5d76a7e43ebc7e1845f2392
Theo Reeves <theo_25@icloud.com>
2024-06-02 15:36:26 +0100

    updating style,JS and read.me

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # Image-Gallery
 
-Week 2 Assignment for Tech Educators to create an accessible gallery website
+!Week 2 Assignment for Tech Educators to create an accessible gallery website
+
 🎯 Display the thumbnail images. Will you use a grid, or a list? Will you use CSS Grid or Flexbox?
 
 🎯 Display the larger image. Will you use an overlay of some kind, or a section on the same page? How would it work on smaller screens?
 
 🎯 Handle user interaction. Will you use event listeners on the thumbnails, or on the container? How will you know which thumbnail was selected?
 
-🎯 Please mention the requirements you met and which goals you achieved for this assignment.
+- Please mention the requirements you met and which goals you achieved for this assignment.
 
-🎯 Were there any requirements or goals that you were not quite able to achieve?
+- Were there any requirements or goals that you were not quite able to achieve?
 
-🎯 If so, could you please tell us what was it that you found difficult about these tasks?
+- If so, could you please tell us what was it that you found difficult about these tasks?
+
+My Reflection.
+
+I really really struggled with this assignment and I spent alot of time going back and forth over what we had done during the week to be able to get some sort of understanding on how to do it and even then at points I felt I was just confusing myself. I feel I have written alot of code in order to be able to achieve what I have done and feel there is a much simpler way of writing the code but I haven't managed to get my head around how to actually write it and when I tried I found things wern't working. I found I could append my thumbanils onto the DOM in the div I wanted but I couldn't get those thumbnails to display in the other div on them DOM, with some help from other members in my group I managed to get this to happen by giving them there own variables and selecting them by id's I had set in the array. Then by targeting these variables I was able to add eventListeners so that when I clicked in each indidual thumbnail image it would change my main display image. I then used this to be able to add other eventListeners to make other events work such as keydown.
+I orignially tried to use grid on where to place the elements on my page but after struggling with this I went back to flex box as I felt I had more of an understanding on how to use it but I do think with some more grid practise I would be able to achive a more efficient placement of all my elements which is something I will keep working on.
+A big problem I ran into was my page loading slowly each time you clicked an image, after trying to resize them on css I found they where still holding alot of data so I formatted them on my mac to a Large Jpeg changing how big each image size was and after doing this it solved this issue for me.
+Another problem I had was using the voice over although it would read my image names and Alt text on my buttons it was just saying button, but using the aria-label I was able to make it say what the buttons where.
+I didn't mind so much using the media Query to make it more responsive on smaller screens but I do think I should of spent more time on this to be able to achieve better responsiveness.
+
+I think I met some of the basic requirements for this assignment but if I am totally honest that would be down to the collaboration I had with some of our group members on the discord channel! Alex was especially helpful in explaining where I have gone wrong and why. I know I need to keep working on JavaScript to be able to write better and more efficient code and in the future i'm determined that i'll be able to achieve this but overall I feel dissappointed in myself that i've struggled to understand what we have learn't during the week to be able to help myslef complete this assignment.
--- app.js
+++ app.js
@@ -1,1 +1,199 @@
-console.log("test");
+const images = [
+  {
+    src: "./Images/butterfly Large.jpeg",
+    alt: "A Brown butterfly sitting on a plant",
+    width: "200px",
+    height: "100px",
+    id: "image1",
+  },
+  {
+    src: "./Images/dandelion Large.jpeg",
+    alt: "clear photo of a dandelion",
+    width: "200px",
+    height: "100px",
+    id: "image2",
+  },
+  {
+    src: "./Images/grey-cat Large.jpeg",
+    alt: "A grey kitten with its mouth open showing it's tongue",
+    width: "200px",
+    height: "100px",
+    id: "image3",
+  },
+  {
+    src: "./Images/sunset1 Large.jpeg",
+    alt: "A bright and colourful Norfolk sunset",
+    width: "200px",
+    height: "100px",
+    id: "image4",
+  },
+  {
+    src: "./Images/sunset2 Large.jpeg",
+    alt: "A clear sunset picture with some cloud rushing out from it",
+    width: "200px",
+    height: "100px",
+    id: "image5",
+  },
+];
+
+let tnail = document.getElementById("thumbnail-container");
+let displayImage = document.getElementById("display");
+
+images.forEach((thumbImg) => {
+  let img = document.createElement("img");
+  img.src = thumbImg.src;
+  img.alt = thumbImg.alt;
+  img.id = thumbImg.id;
+  img.tabIndex = 1;
+  tnail.appendChild(img);
+  //   img.addEventListener("click", () => {
+  //     displayImage.src = images.src;
+});
+//   tnail.addEventListener("click", function () {
+//     createLargeImage(thumbImg[images]);
+// });
+// });
+
+// console.log(image1, image2);
+
+//Making my display Image
+function myFunction() {
+  let img = document.createElement("img");
+  img.src = images[0].src;
+  img.alt = images[0].alt;
+  img.id = "bigImage";
+  displayImage.appendChild(img);
+}
+myFunction();
+bigImage = document.querySelector("#bigImage");
+console.log(bigImage);
+
+let image1 = document.querySelector("#image1");
+let image2 = document.querySelector("#image2");
+let image3 = document.querySelector("#image3");
+let image4 = document.querySelector("#image4");
+let image5 = document.querySelector("#image5");
+
+//adding event listeners for each thumbnail to display on my big Image.
+
+image1.addEventListener("click", () => {
+  bigImage.src = image1.src;
+});
+image2.addEventListener("click", () => {
+  bigImage.src = image2.src;
+});
+image2.addEventListener("click", () => {
+  bigImage.src = image2.src;
+});
+image3.addEventListener("click", () => {
+  bigImage.src = image3.src;
+});
+image4.addEventListener("click", () => {
+  bigImage.src = image4.src;
+});
+image5.addEventListener("click", () => {
+  bigImage.src = image5.src;
+});
+image1.addEventListener("keypress", () => {
+  if (event.key === "Enter") {
+    bigImage.src = image1.src;
+  }
+});
+image2.addEventListener("keypress", () => {
+  if (event.key === "Enter") {
+    bigImage.src = image2.src;
+  }
+});
+image3.addEventListener("keypress", () => {
+  if (event.key === "Enter") {
+    bigImage.src = image3.src;
+  }
+});
+image4.addEventListener("keypress", () => {
+  if (event.key === "Enter") {
+    bigImage.src = image4.src;
+  }
+});
+image5.addEventListener("keypress", () => {
+  if (event.key === "Enter") {
+    bigImage.src = image5.src;
+  }
+});
+
+//Making arrow keys work to cycle through images.
+
+document.addEventListener("keydown", () => {
+  if (event.key === "ArrowRight") {
+    if (bigImage.src == image1.src) {
+      bigImage.src = image2.src;
+    } else if (bigImage.src == image2.src) {
+      bigImage.src = image3.src;
+    } else if (bigImage.src == image3.src) {
+      bigImage.src = image4.src;
+    } else if (bigImage.src == image4.src) {
+      bigImage.src = image5.src;
+    } else if (bigImage.src == image5.src) {
+      bigImage.src = image1.src;
+    }
+  }
+});
+document.addEventListener("keydown", () => {
+  if (event.key === "ArrowLeft") {
+    if (bigImage.src == image1.src) {
+      bigImage.src = image5.src;
+    } else if (bigImage.src == image2.src) {
+      bigImage.src = image1.src;
+    } else if (bigImage.src == image3.src) {
+      bigImage.src = image2.src;
+    } else if (bigImage.src == image4.src) {
+      bigImage.src = image3.src;
+    } else if (bigImage.src == image5.src) {
+      bigImage.src = image4.src;
+    }
+  }
+});
+
+// Make my buttons work
+
+let forwardButton = document.querySelector(".forbutton");
+let backwardButton = document.querySelector(".backbutton");
+console.log(forwardButton);
+console.log(backwardButton);
+
+forwardButton.addEventListener("click", () => {
+  if (bigImage.src == image1.src) {
+    bigImage.src = image2.src;
+  } else if (bigImage.src == image2.src) {
+    bigImage.src = image3.src;
+  } else if (bigImage.src == image3.src) {
+    bigImage.src = image4.src;
+  } else if (bigImage.src == image4.src) {
+    bigImage.src = image5.src;
+  } else if (bigImage.src == image5.src) {
+    bigImage.src = image1.src;
+  }
+});
+backwardButton.addEventListener("click", () => {
+  if (bigImage.src == image1.src) {
+    bigImage.src = image5.src;
+  } else if (bigImage.src == image2.src) {
+    bigImage.src = image1.src;
+  } else if (bigImage.src == image3.src) {
+    bigImage.src = image2.src;
+  } else if (bigImage.src == image4.src) {
+    bigImage.src = image3.src;
+  } else if (bigImage.src == image5.src) {
+    bigImage.src = image4.src;
+  }
+});
+
+// img.addEventListener("click", () => {
+//   displayImage.src = images.src;
+// });
+
+// images.forEach((displayImg) => {});
+// img = document.createElement("img");
+// img.src = displayImg.src;
+// img.alt = displayImg.alt;
+// img.id = displayImg.id;
+// displayImage.appenchild(img);
